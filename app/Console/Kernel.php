9268cbc963f2857587b7d70f7a52ebf593213588
TJ Ferreira <github@timferreira.com>
2018-08-28 12:54:56 -0700

    Remove Schedule - now that final results are available

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('digistack:check-sos')->everyFiveMinutes()->withoutOverlapping();
+        //$schedule->command('digistack:check-sos')->everyFiveMinutes()->withoutOverlapping();
     }
 
     /**
